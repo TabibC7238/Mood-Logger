e88e2b993d94d371e8504ae18941a3121f040c88
TabibC7238 <chowdhurytabib270@gmail.com>
2025-05-24 22:01:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from datetime import datetime, date
 
 # ---- GOOGLE SHEETS AUTH ----
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+creds_dict = json.loads(st.secrets["GOOGLE_CREDS"])
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 sheet = client.open("Mood Logger").sheet1
 
